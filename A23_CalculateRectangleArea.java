7e07b3bf55a04db009ace6bd6f2b76b77a23dc7d
Natt731 <nataliofelicianosolis@gmail.com>
2024-10-31 02:26:04 +0800

    added calculate rectangle area

---
+++ A23_CalculateRectangleArea.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class A23_CalculateRectangleArea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // This is a Java program that calculates the area of a rectangle given its width and height.
+        
+        scanner.close();
+    }
+}
