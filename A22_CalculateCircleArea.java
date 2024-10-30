c0eb6315e6b169747e022575105f4415f14acb6a
Natt731 <nataliofelicianosolis@gmail.com>
2024-10-30 23:31:46 +0800

    add calculate circle area

---
+++ A22_CalculateCircleArea.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class A22_CalculateCircleArea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // This is a Java program that calculates the area of a circle given its radius.
+        System.out.printf("Enter the radius of a circle: ");
+        int radius = scanner.nextInt();
+
+        double area = 3.14 * radius * radius;
+        System.out.println("The area of the circle is: " + area);
+
+        scanner.close();
+    }
+}
