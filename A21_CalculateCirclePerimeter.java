0789473415c374fae063642ff9bdde0d2df65c6a
Natt731 <nataliofelicianosolis@gmail.com>
2024-10-30 21:57:26 +0800

    Commited

--- A21_CalculateCirclePerimeter.java
+++ A21_CalculateCirclePerimeter.java
@@ -3,9 +3,15 @@ import java.util.*;
 public class A21_CalculateCirclePerimeter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-            System.out.println("circle");
+
+        // This is a Java program that calculates the perimeter of a circle given its radius.
+
+            System.out.printf("Enter the radius of a circle: "); // Prompts the user to enter the radius of a circle. 
+            double radius = scanner.nextDouble(); // Declared a variable double to store the radius of a circle.
+
+            double perimeter = 2 * 3.14 * radius; // Formula for the perimeter of a circle.
+            System.out.println("The perimeter of the circle is: " + perimeter); // Prints the perimeter.
 
             scanner.close();
-
     }
 }
