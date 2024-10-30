2d1e6a91a729bdd2b3a4a469756b59480ad2d96c
Natt731 <nataliofelicianosolis@gmail.com>
2024-10-31 03:11:34 +0800

    Add A24_CalculateTriangleArea.java to calculate triangle area

---
+++ A24_CalculateTriangleArea.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class A24_CalculateTriangleArea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // This is a Java program that calculates the area of a triangle given its base and height.
+        double base, height, area;  
+
+        System.out.printf("Enter the base width: ");    
+        base = scanner.nextDouble();
+        System.out.printf("Enter the height: ");    
+        height = scanner.nextDouble();
+
+        area = (base * height) / 2; 
+
+        System.out.printf("Area of triangle: " + area);    
+        
+        scanner.close();
+    }
+}
