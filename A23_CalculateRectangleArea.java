fb38e47089d029f5422f85220fa9cdd6efe74267
Natt731 <nataliofelicianosolis@gmail.com>
2024-10-31 02:33:57 +0800

    commited area of a rectangle

--- A23_CalculateRectangleArea.java
+++ A23_CalculateRectangleArea.java
@@ -4,8 +4,18 @@ public class A23_CalculateRectangleArea {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        // This is a Java program that calculates the area of a rectangle given its width and height.
-        
+        // This is a Java program that calculates the area of a rectangle given its length and width.
+
+        double length, width, area;
+        System.out.printf("Enter length of the rectangle: ");
+        length = scanner.nextDouble();
+        System.out.printf("Enter width of the rectangle: ");
+        width = scanner.nextDouble();
+
+        area = length * width;
+
+        System.out.println("Area of rectangle: " + area);
+
         scanner.close();
     }
 }
